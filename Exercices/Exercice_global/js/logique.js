bee2b2adc259c4faaacd2e49d3619e5f9d829da9
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-10-04 15:08:29 +0200

    suite exercice global --> exo 5

--- Exercices/Exercice_global/js/logique.js
+++ Exercices/Exercice_global/js/logique.js
@@ -5,13 +5,13 @@ function makeblue() {
 
     div1.classList.add('stdiv1')
 }
-
+//Exo 2:
 div2.addEventListener("click", deletestyle)
 
 function deletestyle() {
     div1.classList.remove('stdiv1')
 }
-
+//Exo 3:
 div3.addEventListener("mouseover", makearial)
 
 function makearial() {
@@ -22,5 +22,23 @@ div3.addEventListener("mouseout", makenormal)
 
 function makenormal() {
     div3.style.fontFamily = body.style.fontFamily
-
 }
+//Exo 4:
+chk41.addEventListener("click", yesmake)
+chk42.addEventListener("click", yesmake)
+
+
+function yesmake() {
+    if (chk41.checked == true && chk42.checked == true) {
+        div4.innerHTML = "YES"
+        div4.classList.add("stdiv4")
+    }
+}
+
+//Exo 5:
+div50.addEventListener("click", detruirediv)
+
+function detruirediv(){
+    div51.style.display = "none"
+}
+
