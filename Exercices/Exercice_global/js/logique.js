0ffc61a800d98d8c9e62569d3e817b55233ddfea
samuelroland <samuel.roland@cpnv.ch>
2019-10-06 18:10:10 +0200

    Exo 6 fini

--- Exercices/Exercice_global/js/logique.js
+++ Exercices/Exercice_global/js/logique.js
@@ -5,12 +5,14 @@ function makeblue() {
 
     div1.classList.add('stdiv1')
 }
+
 //Exo 2:
 div2.addEventListener("click", deletestyle)
 
 function deletestyle() {
     div1.classList.remove('stdiv1')
 }
+
 //Exo 3:
 div3.addEventListener("mouseover", makearial)
 
@@ -23,6 +25,7 @@ div3.addEventListener("mouseout", makenormal)
 function makenormal() {
     div3.style.fontFamily = body.style.fontFamily
 }
+
 //Exo 4:
 chk41.addEventListener("click", yesmake)
 chk42.addEventListener("click", yesmake)
@@ -38,7 +41,28 @@ function yesmake() {
 //Exo 5:
 div50.addEventListener("click", detruirediv)
 
-function detruirediv(){
+function detruirediv() {
     div51.style.display = "none"
 }
 
+//Exo 6:
+
+//ajouter levenement keyup (quand une touche remonte):
+inpName.addEventListener("keyup", createinitials)
+
+function createinitials() {
+    //Permet de générer les initiales au format CPNV = Première lettre du prénom + Première lettre du nom + Dernière lettre du nom.
+
+    //prendre le premier caractère pour la première lettre des initiales.
+    initials = inpName.value.substring(0, 1)
+    if (inpName.value.indexOf(" ") != -1) {    //si il y a un espace alors on peut calculer la suite des lettres:
+        //prendre la première lettre après l'espace pour avoir la deuxieme lettre des initiales
+        initials += inpName.value.substring(inpName.value.indexOf(" ") + 1, inpName.value.indexOf(" ") + 2)
+
+        if (inpName.value.indexOf(" ") + 2 < inpName.value.length) { //si il y a deux caractères après l'espace:
+            initials += inpName.value.substring(inpName.value.length - 1, inpName.value.length)   //ajouter 3eme lettre.
+        }
+    }
+    initials = initials.toUpperCase()   //Mettre la chaine en majuscule
+    inpInitials.value = initials    //afficher dans la textbox.
+}
