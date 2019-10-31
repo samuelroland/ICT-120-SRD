c396f35e76f46bb0e7205e2ba7443bce07530a40
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-10-31 16:58:02 +0100

    WIP tableau design

--- SiteVoyagePekin/js/logique.js
+++ SiteVoyagePekin/js/logique.js
@@ -3,3 +3,12 @@ Projet: code Javascript du site du voyage à Pékin
 Auteur: Samuel Roland
 Date: octobre 2019
 */
+prenom.addEventListener("click", prenomclick)
+nom.addEventListener("click", prenomclick)
+age.addEventListener("click", prenomclick)
+
+function prenomclick(objecttxt){
+
+     test= objecttxt.target //prendre l'objet qui a déclenché l'évenement.
+    test.innerHTML = "outch"
+}
