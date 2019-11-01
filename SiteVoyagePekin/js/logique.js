fc0f059767a9570bf75325ab8ca90cb019802044
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-11-01 16:05:40 +0100

    wip fleche hautbas conf tableau entete

--- SiteVoyagePekin/js/logique.js
+++ SiteVoyagePekin/js/logique.js
@@ -7,8 +7,24 @@ prenom.addEventListener("click", prenomclick)
 nom.addEventListener("click", prenomclick)
 age.addEventListener("click", prenomclick)
 
-function prenomclick(objecttxt){
+//constantes valeurs chars spéciaux:
+flhautbas = "&varr;"
+flhaut = "&darr;"
+flbas = ""
 
-     test= objecttxt.target //prendre l'objet qui a déclenché l'évenement.
-    test.innerHTML = "outch"
+function prenomclick(objecttxt) {
+
+    test = objecttxt.target //prendre l'objet qui a déclenché l'évenement.
+    test.innerHTML = test.innerHTML.substring(0, test.innerHTML.indexOf(" ") + 1)
+    //Ajouter flèche hautbas, haut, bas.
+    if (test.innerHTML.indexOf(flhautbas)) {
+        test.innerHTML += flhaut
+    }
+    if (test.innerHTML.indexOf(flhaut)) {
+        test.innerHTML += flbas
+    }
+    if (test.innerHTML.indexOf(flbas)) {
+        test.innerHTML += flhautbas
+    }
 }
+
