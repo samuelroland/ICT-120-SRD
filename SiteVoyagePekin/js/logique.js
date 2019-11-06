0479fd0e751c6c924985a1a05137831a59e74313
samuelroland <samuel.roland@cpnv.ch>
2019-11-07 00:23:01 +0100

    début modification du contenu du tableau.

--- SiteVoyagePekin/js/logique.js
+++ SiteVoyagePekin/js/logique.js
@@ -36,34 +36,36 @@ function prenomclick(event) {
 
 bnnewrow.addEventListener("click", newrow)
 
-function newrow(){
-    zonecols= document.getElementsByTagName("tbody")[0]
-    row= document.createElement("tr")
+function newrow() {
+    zonecols = document.getElementsByTagName("tbody")[0]
+    row = document.createElement("tr")
 
 
     zonecols.appendChild(row)
-    for (i=0;i<tableau.rows[0].cells.length;i++){
+    for (i = 0; i < tableau.rows[0].cells.length; i++) {
         cell = document.createElement("td")
-        cell.id = i+":"+tableau.rows.length
+        cell.id = i + ":" + tableau.rows.length
         row.appendChild(cell)
-        cell.innerText = (cell.id+ "salut")
+        cell.innerText = (cell.id + "")
         cell.style.height = 10
     }
 
 }
+
 var nbslinetbl
 var nbscolstbl
-function updateinfostable(){
-    nbslinetbl= tableau.rows.length
+
+function updateinfostable() {
+    nbslinetbl = tableau.rows.length
     nbscolstbl = tableau.rows[0].cells.length
 }
 
 console.log(document.getElementsByTagName("input"))
 
-function activateedition(){
+function activateedition() {
     //mettre tout les champs input txtbox des colonnes prénom, nom et age en disabled=false
-    for (i=0;i<nbscolstbl;i++){
-        for (j=0;j<nbslinetbl;j++){
+    for (i = 0; i < nbscolstbl; i++) {
+        for (j = 0; j < nbslinetbl; j++) {
 
         }
         for (var cool in tableau.rows[i].cells) {
@@ -76,28 +78,64 @@ function activateedition(){
 
 submit.addEventListener("click", gerercolonnes)
 
-function gerercolonnes(){
+function gerercolonnes() {
     submit.style.backgroundColor = red
     tableau.rows[0].cells.delete();
 }
 
-active=false
+active = false
 bnactiveredition.addEventListener("click", activeedition)
 
-function activeedition(){
-    tds = tableau.getElementsByTagName("td")
-    if (active==false){
-        tableau.classList.remove("table", "table-hover", "table-bordered", "table-striped")
-        for ( i=0;i<5;i++){
-            for (j=0; j<nbslinetbl;j++){
 
+function activeedition() {
+    tds = tableau.getElementsByTagName("td")
+    if (active == false) {
+
+        tableau.classList.remove("table", "table-hover", "table-bordered", "table-striped")
+
+        for (nbline = 0; nbline < cortable.children.length; nbline++) {
+            row = cortable.children[nbline]    //prend la colonne row avec chaque enfant.
+            for (nbcol = 0; nbcol < row.children.length; nbcol++) {
+                cell = row.children[nbcol]
+                inp = document.createElement("input")
+                inp.type = "text"
+                inp.value = cell.innerText
+                cell.innerText = ""
+                cell.appendChild(inp)
             }
-        } 
-        
+        }
+
         active = true
-    } else  {
+    } else {
         tableau.classList.add("table", "table-hover", "table-bordered", "table-striped")
+
+        for (nbline = 0; nbline < cortable.children.length; nbline++) {
+            row = cortable.children[nbline]    //prend la colonne row avec chaque enfant.
+            for (nbcol = 0; nbcol < row.children.length; nbcol++) {
+                cell = row.children[nbcol]
+                cell.innerHTML = cell.firstChild.value
+            }
+        }
         active = false
+    }
+
+}
+
+chkPrenom.addEventListener("click", chkclick)
+chkNom.addEventListener("click", chkclick)
+chkAge.addEventListener("click", chkclick)
+chkAcro.addEventListener("click", chkclick)
+chkIntra.addEventListener("click", chkclick)
+chkSelect.addEventListener("click", chkclick)
+
+function chkclick(event) {
+    obj = event.target
+    colref = obj.name.substr(obj.name.indexOf("chk") + 3).toLowerCase()
+
+    if (obj.checked === false) {
+        document.getElementById(colref).hidden = true
+    } else {
+        document.getElementById(colref).hidden = false
     }
 
 }
